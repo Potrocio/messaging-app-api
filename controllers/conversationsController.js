caa7ca870bc2892a3d4faf2588d05e9bb8e10a5e
Potrocio <57481913+Potrocio@users.noreply.github.com>
2025-07-04 20:23:36 -0400

    Finish conversation router and controller

--- controllers/conversationsController.js
+++ controllers/conversationsController.js
@@ -2,12 +2,110 @@ const { PrismaClient } = require('@prisma/client')
 
 const prisma = new PrismaClient()
 
-function test(req, res) {
-    res.status(200).json({
-        message: "Conversations controller works"
-    })
+async function retrieveConversationUsingKey(req, res) {
+    try {
+        const { userKeyPair } = req.params;
+        if (!userKeyPair) return res.status(400).json({ message: "Missing user pair key" })
+
+        const conversation = await prisma.conversation.findUnique({
+            where: {
+                userKeyPair
+            }
+        })
+
+        if (!conversation) return res.status(404).json({ message: "Conversation not found" })
+
+        return res.status(200).json({ conversation })
+
+    } catch (error) {
+        console.log("retrieveConversationUsingKey", error)
+        res.status(503).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+async function retrieveConversationUsingId(req, res) {
+    try {
+        const id = Number(req.params.id);
+        if (isNaN(id)) {
+            return res.status(400).json({ message: "Invalid ID" });
+        }
+
+        const conversation = await prisma.conversation.findUnique({
+            where: {
+                id
+            }
+        })
+
+        if (!conversation) return res.status(404).json({ message: "Conversation not found" })
+
+        return res.status(200).json({ conversation })
+
+    } catch (error) {
+        console.log("retrieveConversationUsingId", error)
+        res.status(503).json({
+            message: "Internal server error"
+        })
+    }
+}
+
+async function createNewMessage(req, res) {
+
+    try {
+        const { userKeyPair } = req.params;
+        const { senderId, content } = req.body;
+
+        if (!userKeyPair || !senderId || !content) return res.status(400).json({ message: "Missing fields" })
+
+        // Extract the user Ids using the userKeyPair as reference and turn into a number
+        const [userA, userB] = userKeyPair.split(',').map((userId) => {
+            return Number(userId)
+        })
+
+        if (isNaN(userA) || isNaN(userB)) {
+            return res.status(400).json({ message: "Invalid user Id" });
+        }
+
+        // Check if the conversation already exists
+        let conversation = await prisma.conversation.findUnique({
+            where: { userKeyPair },
+            select: { id: true }
+        })
+
+        // If conversation userKeyPair doesn't exist then create a new conversation Id with userA, userB, userKeyPair
+        if (!conversation) {
+            conversation = await prisma.conversation.create({
+                data: {
+                    userA,
+                    userB,
+                    userKeyPair
+                },
+                select: { id: true }
+            })
+        }
+
+        // Create new message using the conversation Id, senderId, content
+        await prisma.message.create({
+            data: {
+                conversationId: conversation.id,
+                senderId,
+                content
+            }
+        })
+
+        return res.status(201).json({ conversation })
+
+    } catch (error) {
+        console.log("createNewMessage", error)
+        res.status(503).json({
+            message: "Internal server error"
+        })
+    }
 }
 
 module.exports = {
-    test
+    retrieveConversationUsingKey,
+    createNewMessage,
+    retrieveConversationUsingId
 }
--- routes/conversationsRouter.js
+++ routes/conversationsRouter.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const conversationsController = require('../controllers/conversationsController')
+const userController = require('../controllers/userController')
 
 const conversationsRouter = express.Router()
 
-conversationsRouter.get('/key/:userPairKey', conversationsController.test) // request conversation using user pair key
-conversationsRouter.post('/key/:userPairKey', conversationsController.test) // create new message
-conversationsRouter.get('/:id', conversationsController.test) // load conversation using conversation id
+conversationsRouter.get('/key/:userKeyPair', userController.authenticateToken, conversationsController.retrieveConversationUsingKey) // request conversation using user key pair, used when clicking on a friend or sending new message
+conversationsRouter.post('/key/:userKeyPair', userController.authenticateToken, conversationsController.createNewMessage) // create new message, and use existing conversation or create new conversation if it doesn't exist
+conversationsRouter.get('/:id', userController.authenticateToken, conversationsController.retrieveConversationUsingId) // load conversation using conversation id, when clicking on conversation preview
 
 module.exports = conversationsRouter;
