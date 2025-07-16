2b768b50237c67497b6f91da6cd4c34dc271334c
Potrocio <57481913+Potrocio@users.noreply.github.com>
2025-07-16 02:10:57 -0400

    Modify funciton to only fetch people that are neither friends or pending friends

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,13 +4,14 @@ const prisma = new PrismaClient();
 
 async function retrieveUsersUsingQueryParameter(req, res) {
     try {
+        const userId = req.user.id
         let { firstName, lastName } = req.query;
-        console.log(firstName, lastName)
 
         if (!firstName || !lastName) return res.status(400).json({ message: "Missing fields" })
 
         firstName = firstName.trim()
         lastName = lastName.trim()
+
 
         const users = await prisma.user.findMany({
             where: {
@@ -33,7 +34,33 @@ async function retrieveUsersUsingQueryParameter(req, res) {
 
         if (users.length === 0) return res.status(404).json({ message: "No users found" })
 
-        return res.status(200).json({ users })
+        const userKeyPairs = users.map((friend) => {
+            const userA = userId < friend.id ? userId : friend.id;
+            const userB = userId > friend.id ? userId : friend.id;
+            return `${userA},${userB}`
+        })
+
+        const knownPeople = await prisma.friend.findMany({
+            where: {
+                userKeyPair: { in: userKeyPairs }
+            },
+            select: {
+                userKeyPair: true
+            }
+        })
+
+        const knownIds = knownPeople.map((keyPair) => {
+            return keyPair.userKeyPair
+        })
+
+
+        const unknownUsers = userKeyPairs.filter((keyPair) => {
+            if (!knownIds.includes(keyPair)) {
+                return keyPair
+            }
+        })
+
+        return res.status(200).json({ unknownUsers })
 
     } catch (error) {
         console.log("retrieveUsersUsingQueryParameter", error)
